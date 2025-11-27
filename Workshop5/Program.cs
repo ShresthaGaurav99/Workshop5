aae02546a448ab8f175ba71d5bfeb0c98c096933
shres <shresthagaurav999@gmail.com>
2025-11-27 10:59:21 +0545

    Task 1 completed

--- Workshop5/Program.cs
+++ Workshop5/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            //----------------Task1------------------
+            // Create BankAccount object
+            BankAccount myAccount = new BankAccount("ACC12345", 5000);
+
+            // Display account number and initial balance
+            Console.WriteLine("Account Number: " + myAccount.AccountNumber);
+            Console.WriteLine("Initial Balance: " + myAccount.Balance);
+
+            // Deposit money
+            myAccount.Deposit(2000);
+
+            // Withdraw money
+            myAccount.Withdraw(1500);
+
+            // Print remaining balance
+            Console.WriteLine("Final Balance: " + myAccount.Balance);
+        }
+    }
+}
